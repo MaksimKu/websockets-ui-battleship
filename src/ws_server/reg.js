33f1f4902fcab58d32e4fd9e887d5c3d6c4e577f
MaksimKu <kuzminowmax@gmail.com>
2023-10-28 23:00:49 +0300

    fix: index

--- src/ws_server/reg.js
+++ src/ws_server/reg.js
@@ -9,15 +9,17 @@ function reg (message) {
     };
     const parseMessage = JSON.parse(message);
     const parseDataMessage = JSON.parse(parseMessage.data);
+    const pass = dataBase.filter(item => item.password === parseDataMessage.password);
     if (dataBase.find((user) => user.name === parseDataMessage.name)) {
+        if (parseDataMessage.password !== pass[0].password) {
         userRegResponseData.error = true;
         userRegResponseData.errorText = 
         'User with this name is already in game. Choose another name';
-        
+        }
     }
     userRegResponseData.name = parseDataMessage.name;
     userRegResponseData.index = dataBase.length;
-    dataBase.push(userRegResponseData);
+    dataBase.push(parseDataMessage);
     return {type: 'reg', data: JSON.stringify(userRegResponseData)}
 }
 
